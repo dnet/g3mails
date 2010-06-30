cdc12931653588d5dc39e9f1484dd15c414fa968
András Veres-Szentkirályi <vsza@vsza.hu>
2010-06-30 21:41:40 +0200

    Finalized export interface

--- getmails.erl
+++ getmails.erl
@@ -18,8 +18,8 @@
 % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 % THE SOFTWARE.
 
--compile(export_all).
 -module(getmails).
+-export([getmails/1]).
 
 -include("atomizer.hrl").
 
